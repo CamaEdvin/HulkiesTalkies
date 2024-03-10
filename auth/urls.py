e0b77cd983db9d8bf81f85a882b3ea0bae97c245
Edvin Cama <edvincama96@gmail.com>
2024-03-10 19:34:08 +0100

    update: register, login: 	deleted:    auth/templates/auth/login.html 	modified:   auth/urls.py 	modified:   auth/views.py 	new file:   templates/login.html 	modified:   templates/register.html

--- auth/urls.py
+++ auth/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView, LoginView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('login/', LoginView.as_view(), name='login'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -4,16 +4,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
-from django.views.generic import View
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.views.generic import TemplateView
 
 
 class RegisterView(APIView):
     template_name = 'register.html'
 
     def get(self, request):
-        # Render the register.html template
         return render(request, self.template_name)
 
     def post(self, request):
@@ -26,3 +23,30 @@ class RegisterView(APIView):
                 'access': str(refresh.access_token),
             }, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(APIView):
+    template_name = 'login.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username and password:
+            try:
+                user = User.objects.get(username=username)
+                if user.check_password(password):
+                    refresh = RefreshToken.for_user(user)
+                    return Response({
+                        'refresh': str(refresh),
+                        'access': str(refresh.access_token),
+                    }, status=status.HTTP_200_OK)
+                else:
+                    return Response({'error': 'Invalid username or password'}, status=status.HTTP_401_UNAUTHORIZED)
+            except User.DoesNotExist:
+                return Response({'error': 'User does not exist'}, status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'error': 'Username and password are required'}, status=status.HTTP_400_BAD_REQUEST)
