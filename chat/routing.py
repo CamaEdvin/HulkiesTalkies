b4a352c8328fc253ef7e23c952df3ae98b8fbfb5
Edvin Cama <edvincama96@gmail.com>
2024-04-02 03:58:09 +0200

    update: 	modified:   chat/routing.py 	modified:   chat/views.py 	modified:   templates/base.html 	modified:   templates/chat/chat.html

--- chat/routing.py
+++ chat/routing.py
@@ -5,6 +5,5 @@ from chat import consumers
 websocket_urlpatterns = [
     re_path(r'private/(?P<room_name>\w+)/$', consumers.PrivateChatConsumer.as_asgi()),
     re_path(r'group/(?P<room_name>\w+)/$', consumers.GroupChatConsumer.as_asgi()),
-    re_path(r'test/$', consumers.TestConsumer.as_asgi()),
 ]
 
--- chat/views.py
+++ chat/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.views import View
 from django.shortcuts import redirect
+from chat import models
 
 
 class NewRoomView(View):
@@ -11,9 +12,36 @@ class NewRoomView(View):
         return render(request, 'chat.html', context)
 
     def post(self, request):
-        selected_user_id = request.POST.get('recipient')
-        return redirect('chat')
-    
+        user_ids = request.POST.getlist('recipients')
+        print("user_ids: ", user_ids)
+
+        usernames = [User.objects.get(id=user_id).username for user_id in user_ids]
+        room_name = ', '.join(usernames)
+        print("room_name: ", room_name)
+
+        if len(user_ids) == 1:
+            room_type = 'private'
+        elif len(user_ids) > 1:
+            room_type = 'group'
+
+        print("room_type: ", room_type)
+
+        room = models.Room.objects.create(name=room_name, room_type=room_type)
+        room_member = models.RoomMember.objects.create(room=room)
+
+        for user_id in user_ids:
+            user = User.objects.get(id=user_id)
+            print("user: ", user)
+            room_member.users.add(user)
+            print("room_member.users: ", room_member.users)
+
+
+        context = {
+            'room_name': room_name,
+            'room_type': room_type,
+        }
+
+        return render(request, 'chat/chat.html', context)
 
 def dashboard(request):
     users = User.objects.all()
