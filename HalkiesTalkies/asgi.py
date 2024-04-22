a3688b351a45eddf0ba55cf438cecbd6fd7509c6
CamaEdvin <camaedvin10@gmail.com>
2024-04-22 23:10:01 +0200

    modified:   HalkiesTalkies/asgi.py

--- HalkiesTalkies/asgi.py
+++ HalkiesTalkies/asgi.py
@@ -16,16 +16,16 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "HalkiesTalkies.settings")
 django_asgi_app = get_asgi_application()
 
-from channels.sessions import SessionMiddlewareStack
+from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
 
 import chat.routing
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        SessionMiddlewareStack(
-            URLRouter(chat.routing.websocket_urlpatterns)
-        )
-    ),
-})
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
+        ),
+    }
+)
