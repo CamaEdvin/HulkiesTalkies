cb6521fd97b914824705939e8f85d57af30c7676
CamaEdvin <camaedvin10@gmail.com>
2024-04-25 03:24:15 +0200

    modified:   chat/consumers.py

--- chat/consumers.py
+++ chat/consumers.py
@@ -23,44 +23,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+
+
+
+
 class PrivateChatConsumer(WebsocketConsumer):
-    def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = "chat_%s" % self.room_name
-
-        # Join room group
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name, self.channel_name
-        )
-
-        self.accept()
-
-    def disconnect(self, close_code):
-        # Leave room group
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name, self.channel_name
-        )
-
-    # Receive message from WebSocket
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name, {"type": "chat_message", "message": message}
-        )
-
-    # Receive message from room group
-    def chat_message(self, event):
-        message = event["message"]
-
-        # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
-
-
-
-"""class PrivateChatConsumer(WebsocketConsumer):
     def connect(self):
         self.username = "Anonymous"
         self.accept(subprotocol='websocket')
@@ -69,6 +36,8 @@ class PrivateChatConsumer(WebsocketConsumer):
         print("self.room_name: ", self.room_name)
         self.room_type = self.scope["url_route"]["kwargs"]["room_type"]
         print("self.room_type: ", self.room_type)
+        self.room_group_name = "chat_%s" % self.room_name
+        print("self.room_group_name: ", self.room_group_name)
         room = self.get_room(self.room_name)
         print("room: ", room)
         if room is None:
@@ -162,7 +131,7 @@ class PrivateChatConsumer(WebsocketConsumer):
             return room
         except models.Room.DoesNotExist:
             return None
-"""
+
 
 """class GroupChatConsumer(mixins.ChatConsumerBase):
     def get_name(self, name):
